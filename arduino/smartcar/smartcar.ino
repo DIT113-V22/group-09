51ec65c1af3cdfb61e79abff6918b8168236461c
noahma <noahma@net.chalmers.se>
2022-04-05 13:57:26 +0200

    finished assignment 1

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,23 @@
 #include <Smartcar.h>
 
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+SR04 sensor (arduinoRuntime, 6, 7, 70);
+SimpleCar car(control);
+
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    car.setSpeed(70);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
+    int distance = sensor.getDistance();
+    if(distance != 0 && distance < 70)
+    {
+        car.setSpeed(0);
+    }
 }
